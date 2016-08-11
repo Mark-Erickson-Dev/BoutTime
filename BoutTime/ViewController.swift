81707ed7ca23928df1285b53e842369361e555b1
Mark-Erickson-Dev <mark.erickson.dev@gmail.com>
2016-08-10 20:49:46 -0500

    Event arrangement

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -26,12 +26,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var instructionLabel: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
     
+    let allEvents = ["Event1", "Event2", "Event3", "Event4", "Event5", "Event6", "Event7", "Event8", "Event9", "Event10", ]
+    
+    let originalEvents = ["Event1", "Event2", "Event3", "Event4"]
+    
+    var userSetEvents = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        userSetEvents = originalEvents
+        
+        setupButtons()
         
         nextRoundButton.hidden = true
         instructionLabel.text = "Shake to complete"
+        
+        setupEvents()
         
     }
 
@@ -43,10 +54,61 @@ class ViewController: UIViewController {
     @IBAction func eventDown(sender: AnyObject) {
         
         let button = sender as! UIButton
-        button.setBackgroundImage(UIImage(named: "down_full_selected"), forState: .Normal)
         
         print("Button \(button.tag) was pressed!")
         
+        var tempEvents = userSetEvents
+        
+        print(tempEvents)
+        
+        let temp = userSetEvents[button.tag + 1]
+        
+        tempEvents.removeAtIndex(button.tag + 1)
+        tempEvents.insert(temp, atIndex: button.tag)
+        
+        print(tempEvents)
+
+        userSetEvents = tempEvents
+        setupEvents()
+    
+    }
+    
+    @IBAction func eventUp(sender: AnyObject) {
+        
+        let button = sender as! UIButton
+        
+        print("Button \(button.tag) was pressed!")
+        
+        var tempEvents = userSetEvents
+        
+        print(tempEvents)
+        
+        let temp = userSetEvents[button.tag - 1]
+        
+        tempEvents.removeAtIndex(button.tag - 1)
+        tempEvents.insert(temp, atIndex: button.tag)
+        
+        print(tempEvents)
+        
+        userSetEvents = tempEvents
+        setupEvents()
+        
+    }
+    
+    func setupEvents() {
+        event1Label.text = userSetEvents[0]
+        event2Label.text = userSetEvents[1]
+        event3Label.text = userSetEvents[2]
+        event4Label.text = userSetEvents[3]
+    }
+    
+    func setupButtons() {
+        event1DownButton.setBackgroundImage(UIImage(named: "down_full_selected"), forState: .Highlighted)
+        event2UpButton.setBackgroundImage(UIImage(named: "up_half_selected"), forState: .Highlighted)
+        event2DownButton.setBackgroundImage(UIImage(named: "down_half_selected"), forState: .Highlighted)
+        event3UpButton.setBackgroundImage(UIImage(named: "up_half_selected"), forState: .Highlighted)
+        event3DownButton.setBackgroundImage(UIImage(named: "down_half_selected"), forState: .Highlighted)
+        event4UpButton.setBackgroundImage(UIImage(named: "up_full_selected"), forState: .Highlighted)
     }
 
 }
